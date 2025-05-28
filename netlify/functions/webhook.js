4537d9e9ede8f3aa5af5703cac043fca521132fc
carzandre-oss <carlos.andre20052003@gmail.com>
2025-05-28 02:22:44 -0300

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,7 +1,8 @@
 const mercadopago = require('mercadopago');
 
 mercadopago.configure({
-    access_token: process.env.MP_ACCESS_TOKEN
+    access_token: process.env.MP_ACCESS_TOKEN,
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
 });
 
 exports.handler = async (event) => {

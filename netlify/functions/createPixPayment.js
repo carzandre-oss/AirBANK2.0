0f68b8242d36475ab86ae2c97629c2b535789f7b
carzandre-oss <carlos.andre20052003@gmail.com>
2025-05-28 02:04:14 -0300

    Update createPixPayment.js

--- netlify/functions/createPixPayment.js
+++ netlify/functions/createPixPayment.js
@@ -1,7 +1,8 @@
 const mercadopago = require('mercadopago');
 
 mercadopago.configure({
-    access_token: process.env.MP_ACCESS_TOKEN
+    access_token: process.env.MP_ACCESS_TOKEN,
+    integratorId: 'dev_24c65fb163bf11ea96500242ac130004',
 });
 
 exports.handler = async (event) => {
